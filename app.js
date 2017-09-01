730c717bb99dd37f3b9accd69bb6c763e2d13046
marco <ti.marco@publicacoesonline.com.br>
2017-09-01 07:30:08 -0300

    colocando porta 300, pq as vezes não funciona o servido se

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 var application = require('./config/server');
 
 //parametrizar a porta que escuta
-application.listen(80, function (){
+application.listen(3000, function (){
     console.log('servidor online');
 })
